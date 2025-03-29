e9a9952a0fdc2db38be9b2d7145a16320c4271ce
Shirin Sinha <150991632+shirinsinha23@users.noreply.github.com>
2025-03-29 17:21:30 +0530

    Add files via upload

---
+++ src/main/java/Tester.java
@@ -0,0 +1,22 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		
+		
+		EntityManagerFactory factory=Persistence.createEntityManagerFactory("emp");
+		EntityManagerFactory entityManagerFactory=factory.createEntityManager();
+		entityManager.getTransaction().begin();
+		Employee employee=new Employee();
+		employee.setName("FromJpa");
+		entityManager.persist(employee);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		factory.close();
+
+	}
+
+}
